6b448764ea386d0c75da8fac3ed782d3eb727ee8
jochemoosterlee-cb <93581578+jochemoosterlee-cb@users.noreply.github.com>
2022-09-05 21:28:49 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,7 @@ if (navigator.mediaDevices.getUserMedia) {
       mimeType: 'video/webm; codecs="vp8"'
     }
     const mediaRecorder = new MediaRecorder(stream, options);
-    console.log(MediaRecorder);
+    console.log(options);
     videoElement.srcObject = stream;
     
 
